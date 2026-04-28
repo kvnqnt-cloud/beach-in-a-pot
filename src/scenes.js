22806cb1ace3d677e4dc32abfaa05aeb8117584f
kvnqnt-cloud <kvn.qnt@gmail.com>
2026-04-28 20:59:09 +0900

    scenes.js

--- src/scenes.js
+++ src/scenes.js
@@ -2,7 +2,7 @@ import { DESIGN_HEIGHT, DESIGN_WIDTH } from "./config.js";
 import { Oil } from "./Oil.js";
 import { ParticleSystem } from "./ParticleSystem.js";
 import { Player } from "./Player.js";
-import { Hud, makeControlsHint, makeHitButton, makeSoundToggle, makeText } from "./UI.js";
+import { Hud, makeHitButton, makeSoundToggle, makeText } from "./UI.js";
 import { clamp, lerp, padScore, rand } from "./utils.js";
 
 function fullSprite(texture) {
@@ -39,7 +39,6 @@ export class MenuScene extends BaseScene {
       this.game.startRun();
     });
 
-    // 🔥 HOVER EFFECT (opacity)
     this.startHit.eventMode = "static";
     this.startHit.cursor = "pointer";
 
@@ -52,9 +51,13 @@ export class MenuScene extends BaseScene {
     });
 
     this.soundToggle = makeSoundToggle(this.game.audio);
-    this.hint = makeControlsHint();
 
-    this.container.addChild(this.bg, this.ui, this.startHit, this.soundToggle, this.hint);
+    this.container.addChild(
+      this.bg,
+      this.ui,
+      this.startHit,
+      this.soundToggle
+    );
   }
 
   async enter() {
@@ -75,13 +78,14 @@ export class GameplayScene extends BaseScene {
     this.bg = fullSprite(game.textures.gameplayBg);
     this.bg2 = fullSprite(game.textures.gameplayBg);
     this.bg2.y = -DESIGN_HEIGHT;
+
     this.oilContainer = new PIXI.Container();
     this.particles = new ParticleSystem();
     this.player = new Player(game.textures.fish, game.textures.deadFish);
+
     this.interface = fullSprite(game.textures.gameplayInterface);
     this.hud = new Hud(game.bestScore);
     this.soundToggle = makeSoundToggle(this.game.audio);
-    this.hint = makeControlsHint();
 
     this.container.addChild(
       this.bg,
@@ -91,8 +95,7 @@ export class GameplayScene extends BaseScene {
       this.player.container,
       this.interface,
       this.hud.container,
-      this.soundToggle,
-      this.hint
+      this.soundToggle
     );
 
     this.score = 0;
@@ -109,36 +112,50 @@ export class GameplayScene extends BaseScene {
     const drift = this.scrollSpeed * 0.055 * dt;
     this.bg.y += drift;
     this.bg2.y += drift;
+
     if (this.bg.y >= DESIGN_HEIGHT) this.bg.y = this.bg2.y - DESIGN_HEIGHT;
     if (this.bg2.y >= DESIGN_HEIGHT) this.bg2.y = this.bg.y - DESIGN_HEIGHT;
   }
 
   update(dt) {
     super.update(dt);
+
     const activeDt = dt * this.slowMo;
+
     this.scrollSpeed = 340 + 420 * (1 - Math.exp(-this.elapsed / 18));
     this.updateBackground(activeDt);
+
     this.score += this.crashed ? 0 : activeDt;
     this.spawnTimer -= activeDt;
 
     const input = this.game.input.right - this.game.input.left;
     this.player.setInput(input);
-    if (input !== 0 && input !== this.previousInput) this.game.audio.blip("move");
+
+    if (input !== 0 && input !== this.previousInput) {
+      this.game.audio.blip("move");
+    }
+
     this.previousInput = input;
 
     this.player.update(activeDt, this.scrollSpeed / 600);
-    if (!this.crashed && Math.random() < activeDt * 8)
+
+    if (!this.crashed && Math.random() < activeDt * 8) {
       this.particles.spawnBubbleTrail(this.player.x, this.player.y, 1);
+    }
 
     if (!this.crashed && this.spawnTimer <= 0) {
       this.spawnOilWave();
-      this.spawnTimer = clamp(1.12 - this.elapsed * 0.022, 0.34, 1.12) + rand(0, 0.22);
+      this.spawnTimer =
+        clamp(1.12 - this.elapsed * 0.022, 0.34, 1.12) +
+        rand(0, 0.22);
     }
 
     for (const oil of this.oils) {
       oil.update(activeDt, this.scrollSpeed, this.elapsed);
-      if (!this.crashed && oil.collides(this.player.getHitCircles()))
+
+      if (!this.crashed && oil.collides(this.player.getHitCircles())) {
         this.handleCrash(oil);
+      }
     }
 
     for (let i = this.oils.length - 1; i >= 0; i -= 1) {
@@ -155,15 +172,26 @@ export class GameplayScene extends BaseScene {
     if (this.crashed) {
       this.crashTimer += dt;
       this.slowMo = lerp(this.slowMo, 0.18, dt * 7);
-      if (this.crashTimer > 0.58) this.game.endRun(this.score);
+
+      if (this.crashTimer > 0.58) {
+        this.game.endRun(this.score);
+      }
     }
   }
 
   spawnOilWave() {
     const count = this.elapsed > 18 && Math.random() < 0.42 ? 2 : 1;
+
     for (let i = 0; i < count; i += 1) {
-      const oil = Oil.randomSpawn(this.game.oilTextures, this.elapsed + i * 2);
-      if (count === 2) oil.container.x += i === 0 ? -260 : 260;
+      const oil = Oil.randomSpawn(
+        this.game.oilTextures,
+        this.elapsed + i * 2
+      );
+
+      if (count === 2) {
+        oil.container.x += i === 0 ? -260 : 260;
+      }
+
       this.oilContainer.addChild(oil.container);
       this.oils.push(oil);
     }
@@ -172,17 +200,25 @@ export class GameplayScene extends BaseScene {
   handleCrash(oil) {
     this.crashed = true;
     this.crashTimer = 0;
+
     this.player.kill();
+
     this.particles.spawnSplash(this.player.x, this.player.y - 30);
+
     oil.sprite.scale.x *= 1.18;
     oil.sprite.scale.y *= 1.1;
+
     this.game.audio.blip("oil");
   }
 
   destroy() {
     this.player.destroy();
     this.particles.destroy();
-    for (const oil of this.oils) oil.destroy();
+
+    for (const oil of this.oils) {
+      oil.destroy();
+    }
+
     super.destroy();
   }
 }
@@ -190,6 +226,7 @@ export class GameplayScene extends BaseScene {
 export class GameOverScene extends BaseScene {
   constructor(game, data) {
     super(game);
+
     this.bg = fullSprite(game.textures.gameplayBg);
 
     this.dead = new PIXI.Sprite(game.textures.deadFish);
@@ -208,7 +245,6 @@ export class GameOverScene extends BaseScene {
       this.game.startRun();
     });
 
-    // 🔥 HOVER EFFECT (opacity)
     this.restartHit.eventMode = "static";
     this.restartHit.cursor = "pointer";
 
@@ -221,7 +257,6 @@ export class GameOverScene extends BaseScene {
     });
 
     this.soundToggle = makeSoundToggle(this.game.audio);
-    this.hint = makeControlsHint();
 
     this.container.addChild(
       this.bg,
@@ -229,15 +264,16 @@ export class GameOverScene extends BaseScene {
       this.interface,
       this.scoreText,
       this.restartHit,
-      this.soundToggle,
-      this.hint
+      this.soundToggle
     );
   }
 
   update(dt) {
     super.update(dt);
+
     this.dead.rotation = Math.sin(this.elapsed * 1.5) * 0.08;
     this.dead.y = 830 + Math.sin(this.elapsed * 1.8) * 10;
+
     this.game.audio.update(dt, this.elapsed, "gameover", 0.42);
   }
 }
